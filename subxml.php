47059aba0a3087744590d017b8d1165b9f866b74
Jack Matier <self@jackalyst.com>
2014-04-23 12:12:41 -0600

    Added sentences. YAY!

--- subxml.php
+++ subxml.php
@@ -4,27 +4,30 @@
  * 
  * Based on the implementation of substr(),
  * but instead of a string it's able to take
- * the inner text of HTML/XML, and instead of
- * allowing you to start anywhere, you're only
- * allowed from the beginning.
+ * the inner text of HTML/XML.
  *
  * @param $string The input string/xml.
  * @param $length The length to trim.
- * @param $boundary Boundary type [BOUNDARY_WORD, BOUNDARY_CHAR]
+ * @param $boundary Boundary type ['char', 'sentence', 'word']
  * @return String
  */
-function subxml($string, $length, $boundary) {
+function subxml($string, $length, $boundary = 'char') {
+	$boundary = (!isset($boundary)) ? 'char' : $boundary;
 	// Set positioning.
-	$pos_char = 0;
+	$pos_char = -1;
 	$pos_word = 0;
+	$pos_sentence = 0;
 
 	// Assign by reference
 	switch($boundary) {
-		case BOUNDARY_CHAR:
+		case 'char':
 			$pos = &$pos_char;			
 		break;
-		case BOUNDARY_WORD:
-			$pos = &$pos_word;	
+		case 'word':
+			$pos = &$pos_word;
+		break;
+		case 'sentence':
+			$pos = &$pos_sentence;
 		break;
 		default:
 			$pos = &$pos_char;
@@ -42,18 +45,29 @@ function subxml($string, $length, $boundary) {
 		// Start HTML sequence.
 		if($string[$i] === '<') { $io_html = true; }
 		
-		// Write character if it's html OR text.
-		if($pos<$length OR $io_html===true) {
-			$new_string[$i] = $string[$i];
-		}
-
-		// Counting.
+		// Counting
 		if($io_html === false) {	
 			$pos_char++;
 			
 			if($string[$i]==" " && $string[$i-1]!==" " && $string[$i+1]!==" ") {
 				$pos_word++;
 			}
+			// Matches for the beginning.
+			if($boundary==='sentence' 
+				&& preg_match('/[a-z](\.|\!\.?)/i', $string[$i-1].$string[$i]) 
+				&& $pos<$length) {
+				
+				while(in_array($string[$i], array('!','?','.'))) {
+					$new_string[$i] = $string[$i];
+					$i++;
+				}
+				$pos_sentence++;
+			}
+		}
+
+		// Write character if it's html OR text.
+		if($pos<$length OR $io_html===true) {
+			$new_string[$i] = $string[$i];
 		}
 
 		// End HTML sequence
